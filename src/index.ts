529b4fd6bd0f062b0aebad34f8c1ef2c815179ab
guilhermejansen <guilhermejansenoficial@gmail.com>
2024-04-10 05:46:11 -0300

    main

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ const dirPath = '/home/deploy/multi100/backend/public';
 
 startCronJobs(dirPath);
 
-
-
 // Agora você pode acessar as variáveis de ambiente com process.env
 console.log(process.env.RETENTION_TIME);
 console.log(process.env.WEBHOOK_URL);
